de9a57ba54babf2f0739c4dd87f6cebae50d35ea
David Borel <dave.borel@gmail.com>
2016-06-05 16:22:48 -0700

    - Check return codes on all LibOVR calls. - Retry initialization until it succeeds.

--- RiftRemote.cpp
+++ RiftRemote.cpp
@@ -5,29 +5,52 @@
 #include <set>
 
 ovrSession g_session = nullptr;
-ovrControllerType g_controllerType = ovrControllerType_Remote;
+ovrControllerType g_controllerType = ovrControllerType_Remote; //TODO: Support Touch/Xbox controller?
 
-void Initialize()
-{
-    ovr_Initialize(nullptr);
-
-	ovrGraphicsLuid luid;
-    ovr_Create(&g_session, &luid);
-}
-
+/// Tear-down logic.
 void Shutdown()
 {
-    ovr_Destroy(g_session);
-    ovr_Shutdown();
-} 
+	// Do nothing if already shutdown.
+	if (g_session == nullptr)
+		return;
 
+    ovr_Destroy(g_session);
+	g_session = nullptr;
+
+    ovr_Shutdown();
+}
+
+/// Start-up logic.
+void Initialize()
+{
+	// Do nothing if already initialized.
+	if (g_session != nullptr)
+		return;
+
+	// Fail now and retry later if initialization fails.
+	if (!OVR_SUCCESS(ovr_Initialize(nullptr)))
+		return;
+
+	// Dummy because you can't pass nullptr.
+	ovrGraphicsLuid luid;
+	memset(&luid, 0, sizeof(ovrGraphicsLuid));
+
+	// Set up the session we will need to query input each frame.
+	if (!OVR_SUCCESS(ovr_Create(&g_session, &luid)))
+		Shutdown();
+}
+
+/// Simulates a Windows keypress.
 void PressWindowsButton(int vk, bool value)
 {
+	// If a key is in this set, it is currently pressed.
     static std::set<int> s_pressedKeys;
 
+	// Do nothing if we are re-setting an already-set state.
     if (value == (s_pressedKeys.count(vk) > 0))
         return;
 
+	// Set up a keyboard event notification.
     INPUT input;
     input.type = INPUT_KEYBOARD;
     input.ki.wScan = 0;
@@ -35,6 +58,7 @@ void PressWindowsButton(int vk, bool value)
     input.ki.dwExtraInfo = 0;
     input.ki.wVk = vk;
 
+	// Set the key as down or up, depending on value.
     if (value)
     {
         input.ki.dwFlags = 0;
@@ -46,9 +70,11 @@ void PressWindowsButton(int vk, bool value)
         s_pressedKeys.erase(vk);
     }
 
+	// Send the event to Windows.
     SendInput(1, &input, sizeof(INPUT));
 }
 
+/// Converts each Oculus input button state to a keyboard press/release in Windows.
 void SendKeysForInputState(const ovrInputState& state)
 {
     PressWindowsButton(VK_BROWSER_FORWARD, (state.Buttons & ovrButton_Up) != 0);
@@ -61,19 +87,23 @@ void SendKeysForInputState(const ovrInputState& state)
 
 int main()
 {
-    Initialize();
-
+	// Register a callback to clean up when the process exits.
     atexit(Shutdown);
 
     while (true)
     {
+		// Retry initialization each frame until it succeeds. It will early-out thereafter.
+		Initialize();
+
+		// Each frame is 10ms to save CPU.
         Sleep(10);
+
+		// Handle button input from LibOVR.
 
         ovrInputState inputState;
         memset(&inputState, 0, sizeof(ovrInputState));
 
-        ovr_GetInputState(g_session, g_controllerType, &inputState);
-
-        SendKeysForInputState(inputState);
+        if (OVR_SUCCESS(ovr_GetInputState(g_session, g_controllerType, &inputState)))
+			SendKeysForInputState(inputState);
     }
 }
